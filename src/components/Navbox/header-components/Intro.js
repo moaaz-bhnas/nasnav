4b1cbedc4e2e7479ffe4b3ba7b56996aab4ff5a3
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-21 11:41:15 +0200

    adjust dashboard badges positions

---
+++ src/components/Navbox/header-components/Intro.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import play from '../../../image/play.svg';
+
+const Intro = () => {
+  return (
+    <div className="intro header__intro header__intro_width_half">
+      <p className="paragraph paragraph_size_xLarge paragraph_color_white paragraph_weight_light intro__paragraph">
+        <strong>Device</strong> download all your products on Google search engine
+      </p>
+      <p className="paragraph paragraph_line_high paragraph_color_white paragraph_letters_spaced paragraph_weight_med intro__paragraph">
+        Our services are not to be reckoned with upload Products can sliping <br /> 
+        in your home and see theyou an edge over the online competitors <br /> 
+        of big businesses data
+      </p>
+      <button className="button intro__button button_color_blue button_back_white button_size_large button_shape_oval">Get Started</button>
+    </div>  
+  );
+}
+
+export default Intro;
