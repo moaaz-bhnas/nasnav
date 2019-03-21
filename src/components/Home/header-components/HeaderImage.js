9202f562db17cd8e58221b36ec29c2396ce36ce9
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-21 09:05:24 +0200

    few adjustments

--- src/components/Home/header-components/HeaderImage.js
+++ src/components/Home/header-components/HeaderImage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import tablet from '../../../image/home-header-tablet.png';
+import phone from '../../../image/home-header-phone.png';
+import './HeaderImage.scss';
 
 const HeaderImage = () => {
   return (
     <div className="headerImageContainer headerImageContainer_width_third">
-      <img className="headerImageContainer__image" src={tablet} alt="Tablet"/>
+      <img className="image headerImageContainer__tablet" src={tablet} alt="Tablet"/>
+      <img className="image headerImageContainer__phone" src={phone} alt="Phone"/>
     </div>
   );
 }
