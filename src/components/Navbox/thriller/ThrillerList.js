c853365f26cfe484710a3df8b0917a3be6813cfb
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-22 09:39:07 +0200

    finished thriller all

--- src/components/Navbox/thriller/ThrillerList.js
+++ src/components/Navbox/thriller/ThrillerList.js
@@ -6,25 +6,25 @@ import thriller3 from '../../../image/thriller3.png';
 
 const ThrillerList = () => {
   return (
-    <ul className="list thrillerList">
+    <ul className="list thrillerList thrillerSection__list">
       <li className="thrillerItem">
         <img className="thrillerItem__image" src={thriller1} alt=""/>
-        <h3>Start Scan</h3>
-        <p className="paragraph paragraph_line_high paragraph_color_grey">
+        <h3 className="thrillerItem__title">Start Scan</h3>
+        <p className="paragraph thrillerItem__paragraph paragraph_line_high paragraph_color_grey paragraph_shortened_small paragraph_centered">
           Do a scan of your products in your shop through your subscription hi with us get your position
         </p>
       </li>
       <li className="thrillerItem">
         <img className="thrillerItem__image" src={thriller2} alt=""/>
-        <h3>Start Scan</h3>
-        <p className="paragraph paragraph_line_high paragraph_color_grey">
+        <h3 className="thrillerItem__title">Elly goes NavBox</h3>
+        <p className="paragraph thrillerItem__paragraph paragraph_line_high paragraph_color_grey paragraph_shortened_small paragraph_centered">
           Do a scan of your products in your shop through your subscription hi with us get your position
         </p>
       </li>
       <li className="thrillerItem">
         <img className="thrillerItem__image" src={thriller3} alt=""/>
-        <h3>Start Scan</h3>
-        <p className="paragraph paragraph_line_high paragraph_color_grey">
+        <h3 className="thrillerItem__title">NavBox&Google</h3>
+        <p className="paragraph thrillerItem__paragraph paragraph_line_high paragraph_color_grey paragraph_shortened_small paragraph_centered">
           Do a scan of your products in your shop through your subscription hi with us get your position
         </p>
       </li>
