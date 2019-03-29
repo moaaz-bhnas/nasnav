c9738cf1493db2ad38950fb1dbc42f7a0a541265
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-29 15:47:55 +0400

    polyfill ie9

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
-import 'react-app-polyfill/ie8';
+import 'react-app-polyfill/ie9';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
