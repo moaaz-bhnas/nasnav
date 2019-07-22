847a2b9af7b347fc0fa7085ebb3de38ee38f4947
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-07-22 02:10:14 +0200

    Added set and map polyfills

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
-import 'react-app-polyfill/ie9';
-import './es6-polyfills';
+import './es6-array-methods-polyfills';
+import 'core-js/features/set';
+import 'core-js/features/map';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
